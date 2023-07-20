8dc6f25ecb1cf0b8e2fa9200d08f49fec51459d0
Emma Liu <yunliu0212@gmail.com>
2023-07-19 17:33:37 -0700

    Add pagination to discussions page

--- src/app/pages/discussions/index.page.ts
+++ src/app/pages/discussions/index.page.ts
@@ -1,9 +1,9 @@
-import {RouteMeta} from '@analogjs/router';
-import {NgFor} from '@angular/common';
-import {Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
-
-import type {Discussion} from '../../lib/github-interfaces';
-import {GithubService} from '../../lib/github.service';
+import { RouteMeta } from '@analogjs/router';
+import { NgFor } from '@angular/common';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
+import type { Discussion } from '../../lib/github-interfaces';
+import { GithubService } from '../../lib/github.service';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 export const routeMeta: RouteMeta = {
@@ -13,7 +13,7 @@ export const routeMeta: RouteMeta = {
 @Component({
   selector: 'app-discussions',
   standalone: true,
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink, MatPaginatorModule],
   template: `
     <header class="mb-4">
       <h1 class="mb-1 text-2xl font-semibold">Discussions</h1>
@@ -27,6 +27,15 @@ export const routeMeta: RouteMeta = {
       <button routerLink={{discussion.number}} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Details</button>
       <hr/>
     </article>
+    <mat-paginator
+        (page)="handlePageEvent($event)"
+        [length]="length"
+        [pageSize]="pageSize"
+        [showFirstLastButtons]="showFirstLastButtons"
+        [pageSizeOptions]="pageSizeOptions"
+        [pageIndex]="pageIndex"
+        aria-label="Select page">
+    </mat-paginator>
     `,
   styles: [
     `
@@ -36,8 +45,17 @@ export const routeMeta: RouteMeta = {
 })
 export default class DiscussionsComponent implements OnInit {
   private github: GithubService = inject(GithubService);
+  private length = 100;
+  private pageSize = 5;
+  private pageIndex = 0;
+  private pageSizeOptions = [5, 10, 25];
+  private showFirstLastButtons = true;
+  private currentPage = 1;
+  private isLastPage = false;
+  private isFinishedLoading = false;
 
   discussions: WritableSignal<Array<Discussion>> = signal([]);
+  allDiscussions: Array<Discussion> = [];
 
   discussionTrackBy(index: number, item: Discussion) {
     return item.number;
@@ -45,11 +63,39 @@ export default class DiscussionsComponent implements OnInit {
 
   ngOnInit() {
     this.github.getDiscussionList()
-        .then(discussions => {
-          this.discussions.set(discussions);
-        })
-        .catch(e => {
-          console.log(e);
-        });
+      .then(discussions => {
+        this.allDiscussions = discussions;
+        this.discussions.set(this.getPageDiscussions(discussions));
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  getPageDiscussions(discussions: Discussion[]) {
+    let firstIndex = 0;
+    let lastIndex = 0;
+    if (!this.isLastPage) {
+      lastIndex = (this.currentPage * this.pageSize);
+      firstIndex = lastIndex - this.pageSize;
+    } else {
+      firstIndex = (this.currentPage - 1) * this.pageSize;
+      lastIndex = this.discussions.length;
+    }
+    let newDiscussionPage: Array<Discussion> = [];
+    discussions.forEach((value, i) => {
+      if (i >= firstIndex && i < lastIndex) {
+        newDiscussionPage.push(value);
+      }
+    });
+    return newDiscussionPage;
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageSize = e.pageSize;
+    this.currentPage = e.pageIndex + 1;
+    const calcLastPage = Math.floor(e.length / e.pageSize);
+    this.isLastPage = calcLastPage === e.pageIndex;
+    this.discussions.set(this.getPageDiscussions(this.allDiscussions));
   }
 }
